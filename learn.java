357fd6dbd34d7c3c34bd364789bebea75c0c3fc6
ca-evian <1485497679@qq.com>
2018-10-20 18:49:58 +0800

    throw_catch

---
+++ learn.java
@@ -0,0 +1,20 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class learn {
+    public static void main(String[] args){
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+        List<Date> list = new ArrayList<>(10);
+        for (int cnt = 0; cnt < 10; cnt++){
+            try {
+                Date date = dateformat.parse("09-12");
+                list.add(date);
+            }catch (ParseException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
